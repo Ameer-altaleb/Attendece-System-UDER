1b625be08b2086471a4b41eb9a09c43557f83dae
Ameer-altaleb <aaltaleb@reliefexperts.org>
2026-02-02 12:22:50 +0300

    Change Supabase key to new publishable key
    Updated Supabase key to the new publishable key.

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,6 +3,6 @@ import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.39.7';
 
 // استبدل YOUR_ACTUAL_ANON_KEY_FROM_SUPABASE بمفتاح anon public الفعلي من لوحة تحكم Supabase
 const supabaseUrl = 'https://riuhndnuziycarmlllzs.supabase.co';
-const supabaseKey = 'YOUR_ACTUAL_ANON_KEY_FROM_SUPABASE'; 
+const supabaseKey = 'sb_publishable_q3BMLdmSMAgzxbreYXlaWg_Nur-Gm5S'; 
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
